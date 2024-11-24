d1b4de225af286d0b1d3c1a3b1e9690261023d6d
lucaslmota <lucaslmota0431@gmail.com>
2024-11-24 15:25:28 -0300

    Template Data Binding Angular, aula 22- Property Binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -10,6 +10,10 @@ export class DataBidingComponent implements OnInit {
   public nome: string = "Lucas"
   public idade: number = 31
 
+  public desabilitado: boolean = false
+
+  public imagemTeste: string = "https://services.meteored.com/img/article/inteligencia-artificial-aprende-a-reconstruir-imagens-vistas-por-pessoas-ciencia-fotos-1679175318563_1024.jpg"
+
   constructor(){
 
   }
